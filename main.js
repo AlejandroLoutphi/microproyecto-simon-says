1339ec17ed87f83dc5fdfcd3ef8ee84c22386996
AlejandroLoutphi <alejandroloutphi@gmail.com>
2025-02-13 18:31:01 -0400

    Guardar y restaurar hiscores Esto es por la API localStorage También ahora se desactivan los botones cuando simonSays()

--- main.js
+++ main.js
@@ -1,55 +1,84 @@
 const mainMenu = document.getElementById('main-menu');
+const nameInput = document.getElementById('name-input');
 const playBtn = document.getElementById('play-btn');
+const scoreDisplay = document.getElementById('score-display');
+const hiscoreDisplay = document.getElementById('hiscore-display');
 const scoreBtn = document.getElementById('score-btn');
 const gameOverScreen = document.getElementById('game-over-screen');
 const game = document.getElementById('game');
-const pauseBtn = document.getElementById('pause-btn');
+const resetBtn = document.getElementById('reset-btn');
 const green = document.getElementById('green');
 const red = document.getElementById('red');
 const yellow = document.getElementById('yellow');
 const blue = document.getElementById('blue');
 const btns = [green, red, yellow, blue];
-let simonSeq = [];
+let simonSeq;
 let btnIdxToEnter = 0;
+let score;
+let hiscore;
+let isSimonSaying = false;
 
-playBtn.addEventListener('click', () => {
+function showGame() {
 	mainMenu.hidden = true;
 	game.hidden = false;
 	gameOverScreen.hidden = true;
-	simonSays();
-});
+}
 
-function gameOver() {
+function showMainMenu() {
+	mainMenu.hidden = false;
+	game.hidden = true;
+	gameOverScreen.hidden = true;
+}
+
+function showGameOverScreen() {
 	mainMenu.hidden = true;
 	game.hidden = true;
 	gameOverScreen.hidden = false;
-	simonSeq = [];
-	setTimeout(() => {
-		mainMenu.hidden = false;
-		game.hidden = true;
-		gameOverScreen.hidden = true;
-	}, 800);
 }
 
 function lightUpBtn(btn) {
-	const bgColor = btn.style.backgroundColor;
-	btn.style.backgroundColor = 'white';
-	setTimeout(() => btn.style.backgroundColor = bgColor, 100);
+	btn.classList.add('game-btn-pressed');
+	setTimeout(() => btn.classList.remove('game-btn-pressed'), 100);
 }
 
 function simonSays() {
 	const waitTime = 400;
 	btnIdxToEnter = 0;
+	score++;
+	scoreDisplay.innerHTML = score;
+	if (score > hiscore) hiscoreDisplay.innerHTML = score;
 	simonSeq = [...simonSeq, Math.floor(Math.random() * 4)];
-	for (let i = 0, len = simonSeq.length; i < len; i++)
-		setTimeout(() => lightUpBtn(btns[simonSeq[i]]), waitTime * i + waitTime);
+	isSimonSaying = true;
+	for (let i = 0; i < simonSeq.length; i++)
+		setTimeout(() => lightUpBtn(btns[simonSeq[i]]), waitTime * (i + 1));
+	setTimeout(() => isSimonSaying = false, waitTime * (simonSeq.length + 1));
 }
 
-btns.forEach((btn, idx) => {
-	btn.addEventListener('click', () => {
-		if (idx !== simonSeq[btnIdxToEnter]) return gameOver();
-		btnIdxToEnter++;
-		lightUpBtn(btn);
-		if (btnIdxToEnter === simonSeq.length) simonSays();
-	});
-});
+function gameOver() {
+	simonSeq = [];
+	showGameOverScreen();
+	window.localStorage.setItem(nameInput.value, score);
+	setTimeout(showMainMenu, 800);
+}
+
+function startGame() {
+	simonSeq = [];
+	score = -1;
+	// si getItem() retorna null, Number() retorna 0, que es lo que queremos
+	hiscore = Number(window.localStorage.getItem(nameInput.value));
+	hiscoreDisplay.innerHTML = hiscore;
+	showGame();
+	simonSays();
+}
+
+btns.forEach((btn, idx) => btn.addEventListener('click', () => {
+	if (isSimonSaying) return;
+	if (idx !== simonSeq[btnIdxToEnter]) return gameOver();
+	btnIdxToEnter++;
+	lightUpBtn(btn);
+	if (btnIdxToEnter === simonSeq.length) simonSays();
+}));
+
+nameInput.addEventListener('input', () => playBtn.disabled = nameInput.value.length === 0);
+playBtn.addEventListener('click', startGame);
+resetBtn.addEventListener('click', gameOver);
